06269b3ff447e32d04d64146766b73035599ac38
ashkan <karimiashkan8181@gmail.com>
2023-03-15 18:43:53 +0330

    complete sms service provider

--- sms/helpers.py
+++ sms/helpers.py
@@ -3,10 +3,17 @@ from django.conf import settings
 
 
 def send_sms(receiver, msg):
-    response = requests.get(
-        f'{settings.SMS_WEB_SERVICE_URL}username={settings.SMS_USERNAME}&password={settings.SMS_PASSWORD}&from={settings.SMS_FROM_NUMBER}&to={receiver}&text={msg}'
-    )
+    # response = requests.get(
+    #     f'{settings.SMS_WEB_SERVICE_URL}username={settings.SMS_USERNAME}&password={settings.SMS_PASSWORD}&from={settings.SMS_FROM_NUMBER}&to={receiver}&text={msg}'
+    # )
 
-    if response.status_code == 200 and response.text == 'SendWasSuccessful':
+    response = requests.get(
+        f'https://api.kavenegar.com/v1/{settings.SMS_WEB_SERVICE_TOKEN}/sms/send.json?receptor={receiver}&message={msg}')
+
+    print('dddddddddddddddddddd')
+    print(response.status_code)
+    print(response.text)
+
+    if response.status_code == 200:
         return False
     return True
