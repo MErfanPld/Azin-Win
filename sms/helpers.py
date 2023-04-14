8a4a082ea94d270aa7d6dd60332703ac9e0250df
ashkan karimi <karimiashkan8186@gmail.com>
2023-04-14 20:54:51 +0430

    complete sms module and features

--- sms/helpers.py
+++ sms/helpers.py
@@ -3,16 +3,24 @@ from django.conf import settings
 from kavenegar import *
 
 
-def send_sms(receiver, msg):
-    # response = requests.get(
-    #     f'{settings.SMS_WEB_SERVICE_URL}username={settings.SMS_USERNAME}&password={settings.SMS_PASSWORD}&from={settings.SMS_FROM_NUMBER}&to={receiver}&text={msg}'
-    # )
+def send_sms(receiver, body_id):
+    data = {
+        'username': settings.SMS_USERNAME,
+        'password': settings.SMS_PASSWORD,
+        'text': f'{receiver}',
+        'to': receiver,
+        'bodyId': body_id,
+    }
+    response = requests.post(settings.SMS_WEB_SERVICE_URL, data)
 
-    api_key = '6A7A663139434F576369452F385972337444784A637A505374637147524C396F6F7A6B4338614333414F453D'
+    # api_key = '6A7A663139434F576369452F385972337444784A637A505374637147524C396F6F7A6B4338614333414F453D'
+    # response = requests.get(f'https://api.kavenegar.com/v1/{api_key}/sms/send.json?receptor=09396988720&message=salam')
 
-    response = requests.get(f'https://api.kavenegar.com/v1/{api_key}/sms/send.json?receptor=09396988720&message=salam')
     print('wwwwwwwwwwwwwwwwwwww')
     print(response.status_code)
     print(response.text)
+
+    if response.status_code and response.json().get('RetStatus') == 1:
+        return False
 
     return True
